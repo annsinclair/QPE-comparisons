5c86754236053db5906c4ad2058e865494785255
Ann Sinclair <144384545+annsinclair@users.noreply.github.com>
2026-04-07 13:46:59 -0500

    Update he_clean.py

--- clean_gridded_qpes/he_clean.py
+++ clean_gridded_qpes/he_clean.py
@@ -7,41 +7,41 @@ import re
 from datetime import datetime
 
 # define paths
-file_path = '/projects/b1045/asinclair/ARs/feb2025/he/he_files'                  # USER INPUT! location of downloaded data
-grid_path = '/projects/b1045/asinclair/ARs/feb2025/he/he_grid_file/NPR.GEO.GHE.v1.Navigation.netcdf.gz'              # USER INPUT! location of downloaded data
-save_path = '/projects/b1045/asinclair/ARs/feb2025/qpe_datasets/he.nc' # USER INPUT! location to store cleaned data
+# USER INPUT! location of downloaded data
+file_path = 'path/to/he_files'
+# USER INPUT! location of downloaded data
+grid_path = 'path/to/he_grid_file'
+# USER INPUT! location to store cleaned data
+save_path = '/path/to/store/cleaned_data/he.nc'
 
-# define boundry conditions (OPTIONAL: these values will crop the data to southern California)
-min_x = 1000                                    # USER INPUT! left boundary
-min_y = 1500                                    # USER INPUT! lower boundary
-max_x = 1500                                    # USER INPUT! right boundary
-max_y = 1900                                    # USER INPUT! upper boundary
+# define boundry conditions (OPTIONAL: these values will crop the data to
+# southern California)
+min_x = 1000                      # USER INPUT! left boundary
+min_y = 1500                      # USER INPUT! lower boundary
+max_x = 1500                      # USER INPUT! right boundary
+max_y = 1900                      # USER INPUT! upper boundary
 
 # open grid dataset
 grid_ds = xr.open_dataset(grid_path)
 # crop grid dataset
 grid_ds = grid_ds.sel(lines=slice(min_x, max_x), elems=slice(min_y, max_y))
-print('grid imported')
 
 # loop over each downloaded files, crop, and set time
 da_list = []
-files = sorted(glob.glob(os.path.join(file_path, "*.nc")))
+files = sorted(glob.glob(os.path.join(file_path, "*.gz")))
 for file in files:
     ds = xr.open_dataset(file)
     da = ds['rain']
-    
+
     da_crop = da.sel(lines=slice(min_x, max_x), elems=slice(min_y, max_y))
-    
-    dt_str = re.search('(\d+)', da_crop.attrs['long_name']).group(0)
+
+    dt_str = re.search('(\\d+)', da_crop.attrs['long_name']).group(0)
     dt = datetime.strptime(dt_str, '%Y%m%d%H%M')
     dt64 = np.datetime64(dt).astype('datetime64[s]')
     da_time = da_crop.expand_dims(dim={'time': [dt64]})
 
     da_list.append(da_time)
 
-print('files imported')
-
-    
 # combine dataarrays into one
 da_full = xr.concat(da_list, dim='time')
 
@@ -50,16 +50,14 @@ ds_merged = xr.merge([grid_ds, da_full]).set_coords(('latitude', 'longitude'))
 
 # get rid of negative (NaN) values
 ds_merged = ds_merged.where(ds_merged['rain'] >= 0.)
-print('files combined')
 
 # rename
-ds_rename = ds_merged.rename({'lines': 'x', 'elems': 'y', 'latitude': 'lat', 'longitude': 'lon'})
+ds_rename = ds_merged.rename(
+    {'lines': 'x', 'elems': 'y', 'latitude': 'lat', 'longitude': 'lon'})
 
 # resample to hourly
 ds_resample = ds_rename.resample(time='1H', label='right').mean()
-print('data resampled to hourly')
 
 # save data as a netCDF
 he_da = ds_resample.rain
 he_da.to_netcdf(save_path)
-print('netCDF saved to', save_path)
