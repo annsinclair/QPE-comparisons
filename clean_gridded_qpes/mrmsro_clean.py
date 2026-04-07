d2b035b5963d80cf4110fa67ce72801481aca5e9
Ann Sinclair <144384545+annsinclair@users.noreply.github.com>
2026-04-07 13:51:10 -0500

    Update mrmsro_clean.py

--- clean_gridded_qpes/mrmsro_clean.py
+++ clean_gridded_qpes/mrmsro_clean.py
@@ -5,51 +5,26 @@ import glob
 import os
 
 # define paths
-file_path = '/projects/b1045/asinclair/ARs/feb2025/mrmsro/mrmsro_files'                  # USER INPUT! location of downloaded data
-save_path = '/projects/b1045/asinclair/ARs/feb2025/qpe_datasets/mrmsro.nc' # USER INPUT! location to store cleaned data
-
-crop = True                                         # USER INPUT! True or False (to crop or not to crop...)
-
-if crop == True:
-    # define bounds of study area, if desired 
-    lon_min = -123                                  # USER INPUT! longitude bound 1 (from -180-180)
-    lon_max = -114                                  # USER INPUT! longitude bound 2 (from -180-180)
-    lat_min = 32                                    # USER INPUT! latitude bound 1 
-    lat_max = 38                                    # USER INPUT! latitude bound 2
-
-# import files 
-files = sorted(glob.glob(os.path.join(file_path, "*.nc")))
-
-# define list for indivual hour data
-da_list = []
-
-# loop through hourly files, clean, add time coordinate
-for file in files:
-    # open dataset
-    ds = xr.open_dataset(file)
-    #select precip variable
-    da = ds.RadarOnlyQPE01H_P0_L102_GLL0
-
-    # crop to study area
-    if crop == True:
-        da = da.where(
-            ((da.lat_0>=lat_min) & (da.lat_0<=lat_max)) & 
-            ((da.lon_0>=(lon_min + 360)) & (da.lon_0<=(lon_max + 360))), drop=True)
-        
-    da_list.append(da)
-print('files imported')
+# USER INPUT! location of downloaded data
+file_path = 'path/to/mrmsro_files'
+# USER INPUT! location to store cleaned data
+save_path = '/path/to/store/cleaned_data/mrmsro.nc'
 
 # open downloaded data files as one dataset
-da_full = xr.concat(da_list, dim='initial_time0_hours')
-print('files combined')
-print('data combined')
+files = glob.glob(os.path.join(file_path, "*.nc"))
+ds = xr.open_mfdataset(files, combine='by_coords')
+print('data successfully imported')
+
+# select just the precip variable
+mrmsro_da = ds.RadarOnlyQPE01H_P0_L102_GLL0
 
 # rename variables
-mrms_da = da_full.rename({'initial_time0_hours': 'time', 'lat_0': 'lat', 'lon_0': 'lon'})
+mrmsro_da = mrmsro_da.rename(
+    {'initial_time0_hours': 'time', 'lat_0': 'lat', 'lon_0': 'lon'})
 
 # change from 360 lon to +/- 180
-mrms_da = mrms_da.assign_coords(lon=(mrms_da.lon - 360))
+mrmsro_da = mrmsro_da.assign_coords(lon=(mrmsro_da.lon - 360))
 
 # save data as a netCDF
-mrms_da.to_netcdf(save_path)
+mrmsro_da.to_netcdf(save_path)
 print('netCDF saved to', save_path)
