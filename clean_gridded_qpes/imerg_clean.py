f39ec823583227deb86e7be91e8b29c9df71440e
Ann Sinclair <144384545+annsinclair@users.noreply.github.com>
2026-04-07 14:35:24 -0500

    Update imerg_clean.py

--- clean_gridded_qpes/imerg_clean.py
+++ clean_gridded_qpes/imerg_clean.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python
+"""
+Filename: imerg_clean.py
+Author: Ann Sinclair
+Date: 2026-04-07
+Version: 1.0
+Description: Clean downloaded IMERG files and save as netCDF files
+"""
+
 # import libraries
 import numpy as np
 import xarray as xr
