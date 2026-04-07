ba562780a89f88a98833a3ef2ec4ca0d29d4d947
Ann Sinclair <144384545+annsinclair@users.noreply.github.com>
2026-04-07 13:51:36 -0500

    Update nldas2_clean.py

--- clean_gridded_qpes/nldas2_clean.py
+++ clean_gridded_qpes/nldas2_clean.py
@@ -5,18 +5,21 @@ import glob
 import os
 
 # define paths
-file_path = '/projects/b1045/asinclair/ARs/feb2025/nldas2/nldas2_ncs'                  # USER INPUT! location of downloaded data
-save_path = '/projects/b1045/asinclair/ARs/feb2025/qpe_datasets/nldas2.nc' # USER INPUT! location to store cleaned data
+# USER INPUT! location of downloaded data
+file_path = 'path/to/nldas2_files'
+# USER INPUT! location to store cleaned data
+save_path = '/path/to/store/cleaned_data/nldas2.nc'
 
 # open downloaded data files as one dataset
-files = sorted(glob.glob(os.path.join(file_path, "*.nc4")))
+files = sorted(glob.glob(os.path.join(file_path, "*.nc")))
 ds = xr.open_mfdataset(files, combine='by_coords')
 print('data successfully imported')
 
 # select just the precip variable
 nldas2_da = ds.Rainf
 
-# transpose variables to match with other products (this makes things easier later)
+# transpose variables to match with other products (this makes things
+# easier later)
 nldas2_da = nldas2_da.transpose('time', 'lat', 'lon')
 
 # save data as a netCDF
